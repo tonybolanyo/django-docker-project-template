03573daf4eaee2252bfd3bab855dc9d2f3d54c04
Tony G. Bolaño <tonybolanyo@gmail.com>
2018-09-19 16:16:14 +0200

    Add JWT urls

--- project_name/urls.py
+++ project_name/urls.py
@@ -17,8 +17,18 @@ from django.conf import settings
 from django.contrib import admin
 from django.urls import include, path
 
+from rest_framework_jwt.views import obtain_jwt_token, refresh_jwt_token, verify_jwt_token
+from rest_framework_swagger.views import get_swagger_view
+
+schema_view = get_swagger_view(title='{{ project_name }} API Doc')
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('health/', include('health_check.urls')),
+    path('api/1.0/auth/login/', obtain_jwt_token, name='api_token_auth'),
+    path('api/1.0/auth/refresh/', refresh_jwt_token, name='api_token_refresh'),
+    path('api/1.0/auth/verify/', verify_jwt_token, name='api_token_verify'),
+    path('', schema_view, name='apidocs'),
 ]
 
 if settings.DEBUG:
